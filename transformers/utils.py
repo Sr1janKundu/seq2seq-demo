77cd1743007e7bcc65cf870f048794e16aa51e11
Srijan Kundu <srijan.kundu7@gmail.com>
2024-10-28 17:48:56 +0530

    Oct 28, 2024

--- transformers/utils.py
+++ transformers/utils.py
@@ -4,21 +4,8 @@ from torchtext.data.metrics import bleu_score
 import sys
 from tqdm import tqdm
 
+
 def translate_sentence(model, sentence, german, english, device, max_length=50):
-    """
-
-    :param model:
-    :param sentence:
-    :param german:
-    :param english:
-    :param device:
-    :param max_length:
-    :return:
-    """
-    # print(sentence)
-
-    # sys.exit()
-
     # Load german tokenizer
     spacy_ger = spacy.load("de_core_news_sm")
 
@@ -28,9 +15,6 @@ def translate_sentence(model, sentence, german, english, device, max_length=50):
     else:
         tokens = [token.lower() for token in sentence]
 
-    # print(tokens)
-
-    # sys.exit()
     # Add <SOS> and <EOS> in beginning and end respectively
     tokens.insert(0, german.init_token)
     tokens.append(german.eos_token)
@@ -41,41 +25,25 @@ def translate_sentence(model, sentence, german, english, device, max_length=50):
     # Convert to Tensor
     sentence_tensor = torch.LongTensor(text_to_indices).unsqueeze(1).to(device)
 
-    # Build encoder hidden, cell state
-    with torch.no_grad():
-        hidden, cell = model.encoder(sentence_tensor)
-
     outputs = [english.vocab.stoi["<sos>"]]
-
-    for _ in range(max_length):
-        previous_word = torch.LongTensor([outputs[-1]]).to(device)
+    for i in range(max_length):
+        trg_tensor = torch.LongTensor(outputs).unsqueeze(1).to(device)
 
         with torch.no_grad():
-            output, hidden, cell = model.decoder(previous_word, hidden, cell)
-            best_guess = output.argmax(1).item()
+            output = model(sentence_tensor, trg_tensor)
 
+        best_guess = output.argmax(2)[-1, :].item()
         outputs.append(best_guess)
 
-        # Model predicts it's the end of the sentence
-        if output.argmax(1).item() == english.vocab.stoi["<eos>"]:
+        if best_guess == english.vocab.stoi["<eos>"]:
             break
 
     translated_sentence = [english.vocab.itos[idx] for idx in outputs]
-
     # remove start token
     return translated_sentence[1:]
 
 
 def bleu(data, model, german, english, device):
-    """
-
-    :param data:
-    :param model:
-    :param german:
-    :param english:
-    :param device:
-    :return:
-    """
     targets = []
     outputs = []
 
